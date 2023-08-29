74e74b7a4069fd508a7ce85744f7eff446d4f9fb
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-08-29 15:12:51 +0100

    Add_var (#6)

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "enable_telemetry" {
+  type        = bool
+  default     = true
+  description = <<DESCRIPTION
+This variable controls whether or not telemetry is enabled for the module.
+For more information see https://aka.ms/avm/telemetry.
+If it is set to false, then no telemetry will be collected.
+DESCRIPTION
+}
+
+# This is required for most resource modules
+variable "resource_group_name" {
+  type        = string
+  description = "The resource group where the resources will be deployed."
+}
