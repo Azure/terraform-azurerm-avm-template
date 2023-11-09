34aa9be27c7021b229ff57037103f9aa7ca62ba5
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2023-11-09 12:50:08 +0100

    fix: example

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -1,11 +1,15 @@
 terraform {
-  required_version = ">= 1.0.0"
+  required_version = ">= 1.3.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.7.0, < 4.0.0"
     }
   }
+}
+
+provider "azurerm" {
+  features {}
 }
 
 variable "enable_telemetry" {
