aefa13e04e03d9f6fffe74f615e4b6d97af687f2
hezijie <lonegunmanb@hotmail.com>
2023-12-19 13:27:34 +0800

    trigger e2e test

--- examples/default2/main.tf
+++ examples/default2/main.tf
@@ -52,6 +52,6 @@ module "test" {
   # source             = "Azure/avm-<res/ptn>-<name>/azurerm"
   # ...
   enable_telemetry    = var.enable_telemetry # see variables.tf
-  name                = ""                   # TODO update with module.naming.<RESOURCE_TYPE>.name_unique
+  name                = "name${random_integer.region_index.result}"
   resource_group_name = azurerm_resource_group.this.name
 }
