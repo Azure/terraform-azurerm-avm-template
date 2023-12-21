51526dadbc6a03282fd82f81bdd20630099767eb
Luke Taylor <77284962+luke-taylor@users.noreply.github.com>
2023-12-21 11:36:00 +0000

    Update locals.telemetry.tf

--- locals.telemetry.tf
+++ locals.telemetry.tf
@@ -28,19 +28,19 @@ locals {
   )
 
   # This is an empty ARM deployment template.
-  telem_arm_template_content = <<TEMPLATE
-{
-  "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
-  "contentVersion": "1.0.0.0",
-  "parameters": {},
-  "variables": {},
-  "resources": [],
-  "outputs": {
-    "telemetry": {
-      "type": "String",
-      "value": "For more information, see https://aka.ms/avm/telemetry"
+  telem_arm_template_content = jsonencode(
+    {
+      "$schema"        = "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+      "contentVersion" = "1.0.0.0",
+      "parameters"     = {},
+      "variables"      = {},
+      "resources"      = [],
+      "outputs" = {
+        "telemetry" = {
+          "type"  = "String",
+          "value" = "For more information, see https://aka.ms/avm/telemetry"
+        }
+      }
     }
-  }
-}
-TEMPLATE
+  )
 }
