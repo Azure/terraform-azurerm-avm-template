a5fe89afd5378f765a33671bf7c51a5f174dd9d9
lonegunmanb <lonegunmanb@hotmail.com>
2024-07-10 15:42:31 +0800

    prepare for new telemetry solution (#124)

--- examples/default/README.md
+++ examples/default/README.md
@@ -11,6 +11,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 3.74"
     }
+    modtm = {
+      source  = "azure/modtm"
+      version = "~> 0.3"
+    }
     random = {
       source  = "hashicorp/random"
       version = "~> 3.5"
@@ -73,6 +77,8 @@ The following requirements are needed by this module:
 - <a name="requirement_terraform"></a> [terraform](#requirement\_terraform) (~> 1.5)
 
 - <a name="requirement_azurerm"></a> [azurerm](#requirement\_azurerm) (~> 3.74)
+
+- <a name="requirement_modtm"></a> [modtm](#requirement\_modtm) (~> 0.3)
 
 - <a name="requirement_random"></a> [random](#requirement\_random) (~> 3.5)
 
--- main.telemetry.tf
+++ main.telemetry.tf
@@ -1,17 +1,24 @@
-resource "random_id" "telem" {
+data "azurerm_client_config" "telemetry" {
   count = var.enable_telemetry ? 1 : 0
-
-  byte_length = 4
 }
 
-# This is the module telemetry deployment that is only created if telemetry is enabled.
-# It is deployed to the resource's resource group.
-resource "azurerm_resource_group_template_deployment" "telemetry" {
+data "modtm_module_source" "telemetry" {
+  count       = var.enable_telemetry ? 1 : 0
+  module_path = path.module
+}
+
+resource "random_uuid" "telemetry" {
+  count = var.enable_telemetry ? 1 : 0
+}
+
+resource "modtm_telemetry" "telemetry" {
   count = var.enable_telemetry ? 1 : 0
 
-  deployment_mode     = "Incremental"
-  name                = local.telem_arm_deployment_name
-  resource_group_name = var.resource_group_name
-  tags                = null
-  template_content    = local.telem_arm_template_content
+  tags = {
+    subscription_id = one(data.azurerm_client_config.telemetry).subscription_id
+    tenant_id       = one(data.azurerm_client_config.telemetry).tenant_id
+    module_source   = one(data.modtm_module_source.telemetry).module_source
+    module_version  = one(data.modtm_module_source.telemetry).module_version
+    random_id       = one(random_uuid.telemetry).result
+  }
 }
