d1810377e07d3e3c227811fe32de8a655c3e8eff
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-08-01 17:49:51 +0100

    Update main.tf

--- examples/default/main.tf
+++ examples/default/main.tf
@@ -24,8 +24,8 @@ provider "azurerm" {
 ## Section to provide a random Azure region for the resource group
 # This allows us to randomize the region for the resource group.
 module "regions" {
-  source  = "Azure/regions/azurerm"
-  version = "~> 0.3"
+  source  = "Azure/avm-utl-regions/azurerm"
+  version = "~> 0.1"
 }
 
 # This allows us to randomize the region for the resource group.
