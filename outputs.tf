64dc43cab6ac7cc448643feb57800a04ca1927e3
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2024-08-13 17:06:15 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -5,9 +5,4 @@ output "private_endpoints" {
   value       = var.private_endpoints_manage_dns_zone_group ? azurerm_private_endpoint.this_managed_dns_zone_groups : azurerm_private_endpoint.this_unmanaged_dns_zone_groups
 }
 
-# Module owners should include the full resource via a 'resource' output
 # https://azure.github.io/Azure-Verified-Modules/specs/terraform/#id-tffr2---category-outputs---additional-terraform-outputs
-output "resource" {
-  description = "This is the full output for the resource."
-  value       = azurerm_resource_group.TODO # TODO: Replace this dummy resource azurerm_resource_group.TODO with your module resource
-}
