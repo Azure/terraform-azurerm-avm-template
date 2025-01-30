99d592fdbc9ecfeb4b86ddb5f19cb9e63694c287
Matt White <16320656+matt-FFFFFF@users.noreply.github.com>
2025-01-30 12:16:57 +0000

    chore: minimums and remove text from readme header (#177)

--- terraform.tf
+++ terraform.tf
@@ -1,10 +1,10 @@
 terraform {
-  required_version = "~> 1.5"
+  required_version = ">= 1.9, < 2.0"
   required_providers {
     # TODO: Ensure all required providers are listed here and the version property includes a constraint on the maximum major version.
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.71"
+      version = "~> 4.0"
     }
     modtm = {
       source  = "azure/modtm"
