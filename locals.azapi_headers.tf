43d88553d918f812edd8e53c312309a6d32688ee
zjhe <lonegunmanb@hotmail.com>
2025-05-12 11:21:11 +0800

    add random uuid into azapi_headers so we can learn count of fork modules

--- locals.azapi_headers.tf
+++ locals.azapi_headers.tf
@@ -8,9 +8,11 @@ locals {
   fork_avm = !anytrue([for r in local.valid_module_source_regex : can(regex(r, one(data.modtm_module_source.telemetry).module_source))])
   # tflint-ignore: terraform_unused_declarations
   azapi_headers = !var.enable_telemetry ? {} : (local.fork_avm ? {
-    fork_avm = "true"
+    fork_avm  = "true"
+    random_id = one(random_uuid.telemetry).result
     } : {
     avm                = "true"
+    random_id          = one(random_uuid.telemetry).result
     avm_module_source  = one(data.modtm_module_source.telemetry).module_source
     avm_module_version = one(data.modtm_module_source.telemetry).module_version
   })
