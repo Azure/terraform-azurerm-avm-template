d3b0ab819f7f87f904bd7afa492995de1504f27c
zjhe <lonegunmanb@hotmail.com>
2025-05-12 11:15:00 +0800

    add locals.azapi_headers so all azapi resource would merge this headers

--- locals.azapi_headers.tf
+++ locals.azapi_headers.tf
@@ -5,11 +5,13 @@ locals {
     "git::https://github\\.com/[A|a]zure/.+",
     "git::ssh:://git@github\\.com/[A|a]zure/.+",
   ]
-  apply_headers = var.enable_telemetry && anytrue([for r in local.valid_module_source_regex : can(regex(r, one(data.modtm_module_source.telemetry).module_source))])
+  fork_avm = !anytrue([for r in local.valid_module_source_regex : can(regex(r, one(data.modtm_module_source.telemetry).module_source))])
   # tflint-ignore: terraform_unused_declarations
-  azapi_headers = local.apply_headers ? {
-    avm = "true"
-    avm_module_source   = one(data.modtm_module_source.telemetry).module_source
-    avm_module_version  = one(data.modtm_module_source.telemetry).module_version
-  } : {}
+  azapi_headers = !var.enable_telemetry ? {} : (local.fork_avm ? {
+    fork_avm = "true"
+    } : {
+    avm                = "true"
+    avm_module_source  = one(data.modtm_module_source.telemetry).module_source
+    avm_module_version = one(data.modtm_module_source.telemetry).module_version
+  })
 }
