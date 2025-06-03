14a696d6305bad43e87bb9160f6ba823f0178d0b
hezijie <lonegunmanb@hotmail.com>
2025-06-03 15:43:49 +0800

    replace azurerm_client_config with azapi_client_config

--- terraform.tf
+++ terraform.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
     }
+    azapi = {
+      source  = "Azure/azapi"
+      version = "~> 2.4"
+    }
     modtm = {
       source  = "azure/modtm"
       version = "~> 0.3"
